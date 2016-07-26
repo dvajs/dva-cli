b88f2398c68c90f8f834be34cc803bcb254b7444
sorrycc <sorrycc@gmail.com>
2016-07-26 13:53:06 +0800

    update boilerplate

--- boilerplates/app/src/routes/IndexPage.jsx
+++ boilerplates/app/src/routes/IndexPage.jsx
@@ -2,20 +2,20 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'dva/router';
 import styles from './IndexPage.less';
 
-function HomePage() {
+function IndexPage() {
   return (
     <div className={styles.normal}>
       <h1>Welcome to dva!</h1>
       <hr />
       <ul className={styles.list}>
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/sorrycc/blog/issues/7" target="_blank">Getting Started</a></li>
+        <li><a href="https://github.com/sorrycc/blog/issues/8" target="_blank">Getting Started</a></li>
       </ul>
     </div>
   );
 }
 
-HomePage.propTypes = {
+IndexPage.propTypes = {
 };
 
-export default HomePage;
+export default IndexPage;
