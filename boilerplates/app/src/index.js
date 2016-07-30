ad79e1e28cc193f6c183d1a33ed071cb84f8d7b9
sorrycc <sorrycc@gmail.com>
2016-07-30 18:26:17 +0800

    update boilerplate

--- boilerplates/app/src/index.js
+++ boilerplates/app/src/index.js
@@ -5,11 +5,14 @@ import dva, { connect } from 'dva';
 // 1. Initialize
 const app = dva();
 
-// 2. Model
+// 2. Plugins
+//app.use();
+
+// 3. Model
 //app.model(require('./models/example'));
 
-// 3. Router
+// 4. Router
 app.router(require('./router'));
 
-// 4. Start
+// 5. Start
 app.start(document.getElementById('root'));
