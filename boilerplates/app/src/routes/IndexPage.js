867fba190717e588f889c3fc4397d83f035c8d6b
sorrycc <sorrycc@gmail.com>
2016-10-20 16:57:32 +0800

    update welcome page

--- boilerplates/app/src/routes/IndexPage.js
+++ boilerplates/app/src/routes/IndexPage.js
@@ -1,13 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
-import styles from './IndexPage.less';
+import styles from './IndexPage.css';
 
 function IndexPage() {
   return (
     <div className={styles.normal}>
-      <h1>Welcome to dva!</h1>
-      <hr />
+      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+      <div className={styles.welcome} />
       <ul className={styles.list}>
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
         <li><a href="https://github.com/sorrycc/blog/issues/8" target="_blank">Getting Started</a></li>
