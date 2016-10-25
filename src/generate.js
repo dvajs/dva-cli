19b3688dc84782e4990cac9b5f9605a67ca04015
sorrycc <sorrycc@gmail.com>
2016-10-25 17:02:33 +0800

    feat: support create route

--- src/generate.js
+++ src/generate.js
@@ -1,35 +1,51 @@
 import api from 'dva-ast/lib/api';
+import upperCamelCase from 'uppercamelcase';
 import { info, error, success } from './log';
 
 const defaultEntry = './src/index.js';
-
-function create(type, payload) {
-  try {
-    api(type, payload);
-  } catch (e) {
-    error(e.stack);
-  }
-}
+const defaultRouter = './src/router.js';
 
 function generate(program, { cwd }) {
   const [type, name] = program.args;
   info('create', `${type} ${name}`);
 
-  switch (type) {
-    case 'model':
-      const modelPath = `./models/${name}`;
-      const filePath = `./src/models/${name}.js`;
-      create('models.create', {
-        namespace: name,
-        sourcePath: cwd,
-        filePath,
-        entry: program.entry || defaultEntry,
-        modelPath,
-      });
-      break;
-    default:
-      error(`ERROR: uncaught type ${type}`);
-      break;
+  try {
+    switch (type) {
+      case 'model':
+        const modelPath = `./models/${name}`;
+        const filePath = `./src/models/${name}.js`;
+        api('models.create', {
+          namespace: name,
+          sourcePath: cwd,
+          filePath,
+          entry: program.entry || defaultEntry,
+          modelPath,
+        });
+        break;
+      case 'route':
+        const componentName = upperCamelCase(name);
+        const componentPath = `./src/routes/${componentName}.js`;
+        api('routeComponents.create', {
+          sourcePath: cwd,
+          filePath: componentPath,
+          componentName,
+        });
+        api('router.createRoute', {
+          filePath: program.router || defaultRouter,
+          sourcePath: cwd,
+          path: `/${name}`,
+          component: {
+            componentName,
+            filePath: componentPath,
+          },
+        });
+        break;
+      default:
+        error(`ERROR: uncaught type ${type}`);
+        break;
+    }
+  } catch (e) {
+    error(e.stack);
   }
 }
 
