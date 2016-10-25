4f36b84739c48f7fb1a84dbae2e54b6efe578b2d
sorrycc <sorrycc@gmail.com>
2016-10-25 17:28:49 +0800

    feat: support generate component

--- src/generate.js
+++ src/generate.js
@@ -1,44 +1,67 @@
 import api from 'dva-ast/lib/api';
 import upperCamelCase from 'uppercamelcase';
 import { info, error, success } from './log';
+import { basename, dirname, join } from 'path';
 
 const defaultEntry = './src/index.js';
 const defaultRouter = './src/router.js';
 
 function generate(program, { cwd }) {
   const [type, name] = program.args;
-  info('create', `${type} ${name}`);
 
   try {
     switch (type) {
       case 'model':
-        const modelPath = `./models/${name}`;
-        const filePath = `./src/models/${name}.js`;
-        api('models.create', {
-          namespace: name,
-          sourcePath: cwd,
-          filePath,
-          entry: program.entry || defaultEntry,
-          modelPath,
-        });
+        (() => {
+          const modelPath = `./models/${name}`;
+          const filePath = `src/models/${name}.js`;
+          const entry = program.entry || defaultEntry;
+          info('create', `model ${name}`);
+          info('register', `to entry ${entry}`);
+          api('models.create', {
+            namespace: name,
+            sourcePath: cwd,
+            filePath,
+            entry,
+            modelPath,
+          });
+        })();
         break;
       case 'route':
-        const componentName = upperCamelCase(name);
-        const componentPath = `./src/routes/${componentName}.js`;
-        api('routeComponents.create', {
-          sourcePath: cwd,
-          filePath: componentPath,
-          componentName,
-        });
-        api('router.createRoute', {
-          filePath: program.router || defaultRouter,
-          sourcePath: cwd,
-          path: `/${name}`,
-          component: {
-            componentName,
+        (() => {
+          const componentName = upperCamelCase(name);
+          const componentPath = `src/routes/${componentName}.js`;
+          info('create', `routeComponent ${componentName}`);
+          api('routeComponents.create', {
+            sourcePath: cwd,
             filePath: componentPath,
-          },
-        });
+            componentName,
+          });
+          info('create', `route ${name} with ${componentPath}`);
+          api('router.createRoute', {
+            filePath: program.router || defaultRouter,
+            sourcePath: cwd,
+            path: `/${name}`,
+            component: {
+              componentName,
+              filePath: componentPath,
+            },
+          });
+        })();
+        break;
+      case 'component':
+        (() => {
+          const fileName = basename(name);
+          const fileDir = dirname(name);
+          const componentName = upperCamelCase(fileName);
+          const filePath = join('src/components', fileDir, `${componentName}.js`);
+          info('create', `component ${filePath}`);
+          api('components.create', {
+            sourcePath: cwd,
+            filePath,
+            componentName,
+          });
+        })();
         break;
       default:
         error(`ERROR: uncaught type ${type}`);
