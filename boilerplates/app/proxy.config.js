d32ad999a8de9b3ee0f5e8a26ab7049a3b9bd6b3
Wei Zhu <yesmeck@gmail.com>
2016-11-14 23:47:17 +0800

    Only load js mock file
    vim created backup files will crash proxy

--- boilerplates/app/proxy.config.js
+++ boilerplates/app/proxy.config.js
@@ -4,7 +4,9 @@ const mock = {};
 
 require('fs').readdirSync(require('path').join(__dirname + '/mock'))
   .forEach(function (file) {
-    Object.assign(mock, require('./mock/' + file));
+    if (/\.js$/.test(file)) {
+      Object.assign(mock, require('./mock/' + file));
+    }
   });
 
 module.exports = mock;
